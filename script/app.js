8f239cc4f5dd9165659b5a54532e77972c50338f
BugRed <o.adrian.rodrigues@gmail.com>
2024-02-20 12:40:37 -0300

    Funções prontas, faltando versão mobile

--- script/app.js
+++ script/app.js
@@ -1,45 +1,42 @@
 
 const criptografar = () => {
+
     const textoEntrada = document.querySelector('.conteudo__input').value;
 
-    const listChar = textoEntrada.split('');
-    const textoEncript = [];
+    if (textoEntrada) {
+        outNotFoud()
+        const listChar = textoEntrada.split('');
+        const textoEncript = [];
 
-    for (const char of listChar) {
-        switch (char) {
-            case 'e':
-                textoEncript.push('enter');
-                break;
-            case 'i':
-                textoEncript.push('imes');
-                break;
-            case 'a':
-                textoEncript.push('ai');
-                break;
-            case 'o':
-                textoEncript.push('ober');
-                break;
-            case 'u':
-                textoEncript.push('ufat');
-                break;
-            default:
-                textoEncript.push(char);
+        for (const char of listChar) {
+            switch (char) {
+                case 'e':
+                    textoEncript.push('enter');
+                    break;
+                case 'i':
+                    textoEncript.push('imes');
+                    break;
+                case 'a':
+                    textoEncript.push('ai');
+                    break;
+                case 'o':
+                    textoEncript.push('ober');
+                    break;
+                case 'u':
+                    textoEncript.push('ufat');
+                    break;
+                default:
+                    textoEncript.push(char);
+            }
         }
+        document.querySelector('.conteudo__div__resp__result').innerHTML = textoEncript.join('');
+        copy();
+
+        document.querySelector('.conteudo__input').value = '';
+
+    } else {
+        showCopy()
     }
-
-    document.querySelector('.conteudo__div__resp__result').innerHTML = textoEncript.join('');
-    if (!textoEntrada) {
-        const area = document.querySelector('.conteudo__div__resp');
-        let text = document.createElement('h1');
-        text.className = 'notFound__text';
-        text.innerHTML = "Não há nenhuma mensagem para criptografar";
-        area.appendChild(text)
-        toggle();
-
-    }
-
-    document.querySelector('.conteudo__input').value = '';
-    copy();
 
 }
 
@@ -47,70 +44,70 @@ const criptografar = () => {
 const descriptografar = () => {
     const textoEntrada = document.querySelector('.conteudo__input').value;
 
-    const textoDescript = textoEntrada
-        .replaceAll('ai', 'a')
-        .replaceAll('enter', 'e')
-        .replaceAll('imes', 'i')
-        .replaceAll('ober', 'o')
-        .replaceAll('ufat', 'u');
+    if (textoEntrada) {
+        outNotFoud()
+        const textoDescript = textoEntrada
+            .replaceAll('ai', 'a')
+            .replaceAll('enter', 'e')
+            .replaceAll('imes', 'i')
+            .replaceAll('ober', 'o')
+            .replaceAll('ufat', 'u');
 
-    document.querySelector('.conteudo__div__resp__result').innerHTML = textoDescript;
-    
-    if (!textoEntrada) {
-        const area = document.querySelector('.conteudo__div__resp');
-        let text = document.createElement('h1');
-        text.className = 'notFound__text';
-        text.innerHTML = "Não há nenhuma mensagem para criptografar";
-        area.appendChild(text)
-        toggle();
+        document.querySelector('.conteudo__div__resp__result').innerHTML = textoDescript;
+
+        document.querySelector('.conteudo__input').value = '';
+
+        copy();
+    } else {
+        showCopy()
     }
-
-    if(document.querySelector('.conteudo__div__resp__copy')){
-        document.querySelector('.conteudo__div__resp__copy').remove();
-    }
-
-    document.querySelector('.conteudo__input').value = '';
-    copy();
-
 
 }
 
 const toggle = () => {
-    const resp = document.querySelector('.conteudo__div__resp')
 
-    let img = document.createElement('img');
-    img.src = "./assets/notFound.svg";
-    img.className = "img";
-    resp.appendChild(img);
+    if (ocument.querySelector('.conteudo__div__resp__copy')) {
+        document.querySelector('.conteudo__div__resp__copy').remove();
+    }
 
 }
 
 
 const copy = () => {
 
-        let btn = document.createElement('button');
-        btn.className = 'conteudo__div__resp__copy';
-        btn.innerHTML = 'Copiar'
-        document.querySelector('.conteudo__div__resp').appendChild(btn);
-
-        btn.onclick = () => {
-            document.querySelector('.conteudo__div__resp__copy').innerHTML = 'Copiado!';
-            setTimeout(() => {
-                document.querySelector('.conteudo__div__resp__copy').innerHTML = 'Copiar';
-            }, 1000);
-        }
+    document.querySelector('.conteudo__div__resp__copy').onclick = () => {
+        document.querySelector('.conteudo__div__resp__copy').innerHTML = 'Copiado!';
+        setTimeout(() => {
+            document.querySelector('.conteudo__div__resp__copy').innerHTML = 'Copiar';
+        }, 1000);
+    }
 
     const value = document.querySelector('.conteudo__div__resp__result').innerHTML;
-    console.log(value)
     navigator.clipboard.writeText(value);
 }
 
 
-const removeImage = () => {
-    let outputImage = document.getElementById('img-output');
-    let outputText = document.getElementById('output-text');
-    outputImage.style.display = 'none';
-    outputText.style.display = 'none';
+const outNotFoud = () => {
+    document.getElementById('copy').style.display = 'inline';
+    document.getElementById('result').style.display = 'inline';
+    document.getElementById('imagem').style.display = 'none';
+    document.getElementById('notFound__text').style.display = 'none';
+}
+
+const showCopy = () => {
+    document.getElementById('imagem').style.display = 'flex';
+    document.getElementById('notFound__text').style.display = 'flex';
+    document.getElementById('copy').style.display = 'none';
+    document.getElementById('result').style.display = 'none';
 }
 
 
+//troca de imagem conforme click
+// let cont = 0;
+
+// const behavior = function() {
+//   btn.addEventListener("click", function(){
+//   count++;
+//   (count % 2 === 0) ? showImage() : hideImage();
+// }
+// behavior();
